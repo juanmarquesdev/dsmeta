d05f31c5962ff925283ddd4f19120bfee35829e8
juanmarquesdev <juanbatistamarques@gmail.com>
2022-09-06 10:43:36 -0300

    Others components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from "/src/assets/img/logo.svg";
+import './styles.css'
+function Header() {
+  return (
+    <>
+      <header>
+        <div className="dsmeta-logo-container">
+          <img src={logo} alt="DSMeta" />
+          <h1>DSMeta</h1>
+          <p>
+            Desenvolvido por
+            <a href="https://github.com/juanmarquesdev">@juanmarquesdev</a>
+          </p>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
